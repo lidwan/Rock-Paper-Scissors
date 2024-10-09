598bfcb3de964a7f4849254dbaafd0ed84d65dec
Loay Idwan <contact@loayidwan.com>
2024-10-09 19:11:08 +0000

    Refactor logic for chosing winner

--- smthn.js
+++ smthn.js
@@ -69,24 +69,18 @@ function calcComputerChoice() {
 
 
 //function to calculate the winner of the round, returns 1 for human, 2 for computer, 3 for tie
-//there's probably a better way to do this, but this is the first thing that came to my mind.
 function calcRoundWinner(humanChoice, computerChoice) {
-    if(humanChoice === "rock" && computerChoice === "scissors")
-        return 1;
-    else if(humanChoice === "rock" && computerChoice === "paper")
-        return 2;
-    else if(humanChoice === "scissors" && computerChoice === "paper")
-        return 1;
-    else if(humanChoice === "scissors" && computerChoice === "rock")
-        return 2;
-    else if(humanChoice === "paper" && computerChoice === "rock")
-        return 1;
-    else if(humanChoice === "paper" && computerChoice === "scissors")
-        return 2;
-    else if(humanChoice === computerChoice) 
-        return 3;
-    else 
-        console.error("something wrong happend while calculating round winner"); //this should never happen, but just in case.
+    
+    if(humanChoice === computerChoice)
+        return 3; //tie
+    
+    if (humanChoice === "rock" && computerChoice === "scissors" || 
+        humanChoice === "scissors" && computerChoice === "paper" || 
+        humanChoice === "paper" && computerChoice === "rock") {
+
+            return 1; //won
+        }
+        return 2; //lost
 }
 
 
